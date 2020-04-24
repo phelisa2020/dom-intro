69522da00c7a596008d0718fc60b7f2e05ccf63d
phelisa2020 <phelisagqwetha@gmail.com>
2020-04-24 15:07:08 +0200

    my commits

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,40 +1,46 @@
-// // get a reference to the textbox where the bill type is to be entered
+var billTypeTextElement = document.querySelector('.billTypeText');   
+var addToBillBtnElement = document.querySelector('.addToBillBtn');   
+var callsTotalElement = document.querySelector('.callTotalOne');  
+var smsTotalElement = document.querySelector('.smsTotalOne');    
+var totalOneElement = document.querySelector('.totalOne');
+var billTypeTextElement = document.querySelector('.billTypeText');
 
-//  //get a reference to the add button
-//  var addToBtnElement = document.querySelector('.addToBillBtn');
-//  var billTypeTextElement =  document.querySelector('.billTypeText');
- 
-//  function textBillTotal (){
-//  	var billTypeTextElement = document.querySelector('.billTypeText');
- 
-//  }
-//  //create a variable that will keep track of the total bill
-//  var totalOneElement = document.querySelector('.tatalOne');
+// 
 
-//  var callsTotalElement = document.querySelector('.callTotalOne');
-//  var smsTotalElement = document.querySelector('.smsTotalOne');
-//  var callsTotal = 0;
-//  var smsTotaL = 0;
+ function textBillTotal(){
 
-//  //add an event listener for when the add button is pressed
-//  var billTypeEntered = billTypeTextElement.value.split(' , ');
-//  for (let i=0; i < billTypeEntered.length; i++ ) {
-//  	var billItem = billTypeEntered[i].trim();
+    // ... other code here
+     var callsTotal = 0;
+    var smsTotal = 0;
+    var billTypeEntered = billTypeTextElement.value.split(",");
+    for(let i=0; i<billTypeEntered.length; i++) {
+        var billItem = billTypeEntered[i].trim();
 
-//  	if(billItem === 'call'){
-//  	 callsTotal += 2.75
-//  	}
-//  	else if (billItem === 'sms'){
-//  		smsTotaL += 0.75
-//  	}
-// }
+    // update the correct total
+    
+    if (billItem === "call"){
+        callsTotal += 2.75
+    }
+    else if (billItem === "sms"){
+        smsTotal += 0.75;
+    }
+}
+    
+    //update the totals that is displayed on the screen.
+    callsTotalElement.innerHTML = callsTotal.toFixed(2);
+    smsTotalElement.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalOneElement.innerHTML = totalCost.toFixed(2);
 
-//  callsTotalElement.innerHTML = callsTotal.toFixed(2);
-//  smsTotalElement.innerHTML= smsTotaL.toFixed(2);
-//  var totalCost = callsTotal + smsTotaL;
-//  totalOneElement.innerHTML = totalCost.toFixed(2);
 
-// // //in the event listener check if the value in the bill type textbox is 'sms' or 'call'
-// // // * add the appropriate value to the running total
-// // // * add nothing for invalid values that is not 'call' or 'sms'.
-// // // * display the latest total on the screen
+    
+    //color the total based on the criteria
+    if (totalCost >= 50){
+        // adding the danger class will make the text red
+        totalOneElement.classList.add("danger");
+    }
+    else if (totalCost >= 30){
+        totalOneElement.classList.add("warning");
+    }
+}
+addToBillBtnElement.addEventListener('click', textBillTotal);
