551841a4accfc26bbda4726b51a5771f926dc942
phelisa2020 <phelisagqwetha@gmail.com>
2020-04-29 11:29:35 +0200

    my commit

--- js/text-bill.js
+++ js/text-bill.js
@@ -3,37 +3,25 @@ var addToBillBtnElement = document.querySelector('.addToBillBtn');
 var callsTotalElement = document.querySelector('.callTotalOne');  
 var smsTotalElement = document.querySelector('.smsTotalOne');    
 var totalOneElement = document.querySelector('.totalOne');
-var billTypeTextElement = document.querySelector('.billTypeText');
 
-// 
 
  function textBillTotal(){
+   
+    var billTypeEntered = billTypeTextElement.value.trim();
 
-    // ... other code here
-     var callsTotal = 0;
-    var smsTotal = 0;
-    var billTypeEntered = billTypeTextElement.value.split(",");
-    for(let i=0; i<billTypeEntered.length; i++) {
-        var billItem = billTypeEntered[i].trim();
-
-    // update the correct total
-    
-    if (billItem === "call"){
+ if (billTypeEntered === "call"){
         callsTotal += 2.75
     }
-    else if (billItem === "sms"){
+    else if (billTypeEntered === "sms"){
         smsTotal += 0.75;
     }
-}
-    
+
     //update the totals that is displayed on the screen.
     callsTotalElement.innerHTML = callsTotal.toFixed(2);
     smsTotalElement.innerHTML = smsTotal.toFixed(2);
     var totalCost = callsTotal + smsTotal;
-    totalOneElement.innerHTML = totalCost.toFixed(2);
+    totalOneElement.innerHTML = totalCost.toFixed(2)
 
-
-    
     //color the total based on the criteria
     if (totalCost >= 50){
         // adding the danger class will make the text red
@@ -43,4 +31,6 @@ var billTypeTextElement = document.querySelector('.billTypeText');
         totalOneElement.classList.add("warning");
     }
 }
+ var callsTotal = 0;
+    var smsTotal = 0;
 addToBillBtnElement.addEventListener('click', textBillTotal);
