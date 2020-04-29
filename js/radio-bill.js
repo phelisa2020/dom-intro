62980f43b5281a97e6201a01bfa4b9d54a8a570e
phelisa2020 <phelisagqwetha@gmail.com>
2020-04-29 16:39:12 +0200

    my commits

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,8 +1,43 @@
 // get a reference to the sms or call radio buttons
+var billItemTypeRadioElement = document.querySelector('.billItemTypeRadio');	
+var radioBillAddBtnElement = document.querySelector('.radioBillAddBtn');
+var callTotalTwoElement	= document.querySelector('.callTotalTwo');
+var smsTotalTwoElement	= document.querySelector('.smsTotalTwo')
+var totalTwoElement = document.querySelector('.totalTwo')
+var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
 
 //get a reference to the add button
+function radioBillTotal (){
 
+var billItemType = billItemTypeRadioElement.value.trim();
+if (billItemType === 'call'){
+	callsTotal += 2.75
+}
+    else if (billItemType === "sms"){
+        smsTotal += 0.75
+    }
+
+    
+    callTotalTwoElement.innerHTML= callsTotal.toFixed(2);
+    smsTotalTwoElement.innerHTML= smsTotal.toFixed(2);
+    var totalCost = callsTotal +  smsTotal;
+    totalTwoElement.innerHTML = totalCost.toFixed(2);
+
+    if(totalCost >= 50){
+    	totalTwoElement.classList.add('danger')
+    }
+
+
+else if(totalCost >= 30){
+	totalTwoElement.classList.add('warning')	
+
+   } // billItemType will be 'call' or 'sms'
+}
 //create a variable that will keep track of the total bill
+var callsTotal = 0;
+var smsTotal = 0;
+radioBillAddBtnElement.addEventListener('click', radioBillTotal);
+
 
 //add an event listener for when the add button is pressed
 
