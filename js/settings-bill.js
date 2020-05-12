944fab0b4c6b87fcf75c27c10e50cffa348ab846
phelisa2020 <phelisagqwetha@gmail.com>
2020-05-12 16:35:49 +0200

    my commits

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -14,58 +14,38 @@ var totalSettingsElement = document.querySelector('.totalSettings');
 var callTotal = 0
 var smsTotal = 0
 
-var callCost = 0
-var smsCost=0
-var warning = 0
-var critical = 0
+var callCost = 0.00;
+var smsCost=0.00;
+var warning = 0.00;
+var critical = 0.00;
+var totalSettings = 0.00
 
-function addBtnClicked (){	
-var billItemTypeRadio = checkedRadioBtn2.value.trim();
-if (billItemTypeRadio === 'call'){
-	callTotal += 2.75
-}
-	else if (billItemTypeRadio === 'sms'){
-		smsTotal +=  0.75
-
-}
-}
-
-
-callTotalSettingsElement.innerHTML= callTotal.toFixed(2);
-smsTotalSettingsElement.innerHTML= smsTotal.toFixed(2);
-var totalCost4 = callTotal +  smsTotal;
-totalSettingsElement.innerHTML = totalCost4.toFixed(2);
-
-
-radioBillAddBtnTwoElement.addEventListener('click', addBtnClicked);
 
 function updateSettingBtnClicked (){
 
-var billItemTypeRadio = checkedRadioBtn2.value.trim();
-
-if (billItemTypeRadio === 'call'){
-	callCost += 2.75
-}
-	else if (billItemTypeRadio === 'sms'){
-		smsCost +=  0.75
-
-}
-
-}
-
-	callCostSettingElement.value= callCost.toFixed(2);
-    smsCostSettingElement.value= smsCost.toFixed(2);
-     var totalCost = callCost +  smsCost;
-     totalSettingsElement.value = totalCost.toFixed(2)
-     warningLevelSettingElement.value = totalCost.toFixed(2)
-     criticalLevelSettingElement.value = totalCost.toFixed(2)
-
-if (totalCost4 >= warning && totalCost4 < critical){
-	totalSettingsElement.className = 'warning'
-}
-
-else if (totalCost4 >= critical){
-	totalSettingsElement.className = 'black'
-
+	  callCost = Number(callCostSettingElement.value) 
+      smsCost = Number(smsCostSettingElement.value)
+      warning = Number(warningLevelSettingElement.value) 
+     critical = Number(criticalLevelSettingElement.value)
+//      changeTotalColor(totalSettings, warning, critical);
 }
 updateSettingElement.addEventListener('click',updateSettingBtnClicked)
+
+function radioBillSetTotal (){
+var checkedRadioBtn2 = document.querySelector("input[name='billItemTypeWithSettings']:checked");
+var billItemTypeRadio = checkedRadioBtn2.value.trim()
+if (billItemTypeRadio === 'call'){
+	callTotal += 2.75
+}
+    else if (billItemTypeRadio === "sms"){
+        smsTotal += 0.75;
+    }
+
+    
+    callTotalSettingsElement.innerHTML= callsTotal2.toFixed(2);
+    smsTotalSettingsElement.innerHTML= smsTotal2.toFixed(2);
+    var totalCost4 = callTotal +  smsTotal;
+    totalSettingsElement.innerHTML = totalCost4.toFixed(2);
+
+}
+radioBillAddBtnTwoElement.addEventListener('click', radioBillSetTotal)
