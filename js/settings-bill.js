ea3512c74c3fba74d8007f6023a25f00604e0143
phelisa2020 <phelisagqwetha@gmail.com>
2020-05-13 12:25:21 +0200

    my commit

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,5 +1,7 @@
 var checkedRadioBtn2 = document.querySelector("input[name='billItemTypeWithSettings']:checked");
 
+//get a reference to the add button
+
 var callCostSettingElement = document.querySelector('.callCostSetting');
 var smsCostSettingElement = document.querySelector('.smsCostSetting');
 var warningLevelSettingElement = document.querySelector('.warningLevelSetting');
@@ -11,14 +13,15 @@ var callTotalSettingsElement = document.querySelector('.callTotalSettings');
 var smsTotalSettingsElement = document.querySelector('.smsTotalSettings');
 var totalSettingsElement = document.querySelector('.totalSettings');
 
-var callTotal = 0
-var smsTotal = 0
+var callTotal = 0.00
+var smsTotal = 0.00
 
 var callCost = 0.00;
 var smsCost=0.00;
 var warning = 0.00;
 var critical = 0.00;
 var totalSettings = 0.00
+var changeTotalColor = 0.00
 
 
 function updateSettingBtnClicked (){
@@ -27,13 +30,14 @@ function updateSettingBtnClicked (){
       smsCost = Number(smsCostSettingElement.value)
       warning = Number(warningLevelSettingElement.value) 
      critical = Number(criticalLevelSettingElement.value)
-//      changeTotalColor(totalSettings, warning, critical);
+changeTotalColor= Number(totalSettings, warning, critical);
 }
 updateSettingElement.addEventListener('click',updateSettingBtnClicked)
 
 function radioBillSetTotal (){
-var checkedRadioBtn2 = document.querySelector("input[name='billItemTypeWithSettings']:checked");
-var billItemTypeRadio = checkedRadioBtn2.value.trim()
+
+var checkedRadioBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
+var billItemTypeRadio = checkedRadioBtn.value.trim()
 if (billItemTypeRadio === 'call'){
 	callTotal += 2.75
 }
